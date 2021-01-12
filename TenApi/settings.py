e6f665db02040a9a0737679cad162c47db823ce4
RohitsSkill <73296362+RohitsSkill@users.noreply.github.com>
2021-01-12 17:59:47 +0530

    Update settings.py

--- TenApi/settings.py
+++ TenApi/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4nvp8w*f4nc=klrodrlc%_r8pf4oemq+0to0+@-=hcw8s-!qa#'
+SECRET_KEY = 'Your Key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
